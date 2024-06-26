f867fd955398fc36fb38e846a163a7cb5afca447
KMjanith <kavinduj.20@cse.mrt.ac.lk>
2024-06-26 17:15:14 +0530

    first commit

---
+++ publish/send.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	// Define the RabbitMQ connection string with the custom username and password
+	conn, err := amqp.Dial("amqp://guest:dkMd9z7k@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// Declare an exchange
+	exchangeName := "GO_exchange"
+	err = ch.ExchangeDeclare(
+		exchangeName, // name
+		"direct",     // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	// Declare a queue
+	queueName := "GO_hello"
+	q, err := ch.QueueDeclare(
+		queueName, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// Bind the queue to the exchange with a routing key
+	routingKey := "GO_routing_key"
+	err = ch.QueueBind(
+		q.Name,       // queue name
+		routingKey,   // routing key
+		exchangeName, // exchange
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to bind a queue")
+
+	// Publish a message to the exchange with the custom routing key
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctx,
+		exchangeName, // exchange
+		routingKey,   // routing key
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
